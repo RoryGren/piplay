809e6a0f2ab9cc1844eeefa6a7c6df408281c2b2
Rory Grenfell <rory@websurfer.co.za>
2022-01-29 17:41:35 +0200

    test lightState

--- lightSensor.py
+++ lightSensor.py
@@ -3,15 +3,27 @@
 import sys
 from gpiozero import LightSensor
 from time import sleep
+from timeit import default_timer as timer
 
 ldr = LightSensor(18)
-
+oldLightState = "off"
+lightState = ""
 
     # Main loop
 while True:
     try:
         sleep(0.05)
-        print(ldr.value)
+        #print(ldr.value)
+        if ldr.value < 0.5:
+            lightState = "off"
+        else:
+            lightState = "on"
+        if oldLightState != lightState:
+            #start = timer()
+            print(lightState)
+            oldLightState = lightState
+        else:
+            print(lightState)
     except KeyboardInterrupt:
         sys.exit("Operation cancelled by user")
     
