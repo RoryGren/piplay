c9df01c07b18ff2be2ddff3ddda4fbe1b708af21
Rory Grenfell <rory@websurfer.co.za>
2022-01-29 21:50:15 +0200

    tidy

--- lightSensor.py
+++ lightSensor.py
@@ -31,16 +31,14 @@ MORSE_CODE_DICT = { 'A':'.-',     'B':'-...',
 
 
 # Main loop
-while receiving:
+while receiving == "true":
     try:
         sleep(0.05)
-        #print(ldr.value)
         if ldr.value < 0.5:
             lightState = "off"
         else:
             lightState = "on"
         if oldLightState != lightState:
-            #print(lightState)
             end = timer()
             duration = end - start
             if oldLightState == "on":
@@ -49,13 +47,16 @@ while receiving:
                 else:
                     flashes += "."
             else:
-                if duration < 1.5:
-                    flashes += " "
-                elif duration > 3:
-                    flashes += "   "
+                if duration > 3:
+                    #end of message
                     receiving = "false"
+                    break
+                if duration < 1.5:
+                    #inter-letter space
+                    flashes += " "
                 else:
-                    flashes = "  "
+                    #inter-word space
+                    flashes += "  "
             oldLightState = lightState
             start = timer()
         #else:
@@ -65,4 +66,46 @@ while receiving:
     except KeyboardInterrupt:
         sys.exit("Operation cancelled by user")
 
+print(flashes)
+message = decrypt(flashes)
+print(message)
+sys.exit()
+
+def decrypt(message):
+ 
+    # extra space added at the end to access the
+    # last morse code
+    message += ' '
+ 
+    decipher = ''
+    citext = ''
+    for letter in message:
+ 
+        # checks for space
+        if (letter != ' '):
+ 
+            # counter to keep track of space
+            i = 0
+ 
+            # storing morse code of a single character
+            citext += letter
+ 
+        # in case of space
+        else:
+            # if i = 1 that indicates a new character
+            i += 1
+ 
+            # if i = 2 that indicates a new word
+            if i == 2 :
+ 
+                 # adding space to separate words
+                decipher += ' '
+            else:
+ 
+                # accessing the keys using their values (reverse of encryption)
+                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
+                .values()).index(citext)]
+                citext = ''
+ 
+    return decipher
 
