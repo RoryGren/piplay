c2b179e22bfc332818c7a6efe2601ff5bc621b9f
Rory Grenfell <rory@websurfer.co.za>
2022-01-29 16:52:56 +0200

    poll timing change

--- lightSensor.py
+++ lightSensor.py
@@ -10,7 +10,7 @@ ldr = LightSensor(18)
     # Main loop
 while True:
     try:
-        sleep(0.01)
+        sleep(0.05)
         print(ldr.value)
     except KeyboardInterrupt:
         sys.exit("Operation cancelled by user")
